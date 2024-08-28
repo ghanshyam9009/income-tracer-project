d688cc2f9789b48cbb631978d577545b7b0a885f
ghanshyam9009 <147299599+ghanshyam9009@users.noreply.github.com>
2024-08-28 17:02:06 +0530

    Update apiURL.js

--- income-expenses-tracker-frontend/src/utils/apiURL.js
+++ income-expenses-tracker-frontend/src/utils/apiURL.js
@@ -1,15 +1,15 @@
-export const API_URL_USER =
-  "http://localhost:8080/api/v1/users";
-export const API_URL_ACC =
-  "http://localhost:8080/api/v1/accounts";
-export const API_URL_TRANSACTION =
-  "http://localhost:8080/api/v1/transactions";
-
 // export const API_URL_USER =
-//   "https://radiant-peak-71363.herokuapp.com/api/v1/users";
+//   "http://localhost:8080/api/v1/users";
 // export const API_URL_ACC =
-//   "https://radiant-peak-71363.herokuapp.com/api/v1/accounts";
+//   "http://localhost:8080/api/v1/accounts";
 // export const API_URL_TRANSACTION =
-//   "https://radiant-peak-71363.herokuapp.com/api/v1/transactions";
+//   "http://localhost:8080/api/v1/transactions";
 
-//https://radiant-peak-71363.herokuapp.com/
+export const API_URL_USER =
+  "https://income-tracer-project.onrender.com/api/v1/users";
+export const API_URL_ACC =
+  "https://income-tracer-project.onrender.com/api/v1/accounts";
+export const API_URL_TRANSACTION =
+  "https://income-tracer-project.onrender.com/api/v1/transactions";
+
+//https://income-tracer-project.onrender.com
